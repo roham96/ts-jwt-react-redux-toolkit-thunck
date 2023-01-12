ff650b7488b266ba4555fc6433419001f9b4f711
roham <rohamshahedi94@gmail.com>
2023-01-12 17:15:54 +0330

    add src

---
+++ README.md
@@ -0,0 +1,74 @@
+### FOLLOW ME 
+#### in:
+- GitHub: <a href="https://github.com/roham96">Roham Shahedi</a>
+- Linkedin: <a href="https://www.linkedin.com/in/roham-shahedi">Roham Shahedi</a>
+# 
+# 
+# Getting Started with Create React App
+
+This project was bootstrapped 
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+#### Here is the project structure for the JWT Authentication & Authorization with React, Redux Toolkit, RTK Query, MUI, and React-Hook-Form.
+
+```sh
+
+#  root tree       
+       
+frontend/
+├── public/
+│ ├── favicon.ico
+│ ├── index.html
+│ ├── logo192.png
+│ ├── logo512.png
+│ ├── manifest.json
+│ └── robots.txt
+├── src/
+│ ├── components/
+│ │ ├── FormInput.tsx
+│ │ ├── FullScreenLoader.tsx
+│ │ ├── Header.tsx
+│ │ ├── layout.tsx
+│ │ └── requireUser.tsx
+│ ├── pages/
+│ │ ├── admin.page.tsx
+│ │ ├── home.page.tsx
+│ │ ├── login.page.tsx
+│ │ ├── profile.page.tsx
+│ │ ├── register.page.tsx
+│ │ ├── unauthorize.page.tsx
+│ │ └── verifyemail.page.tsx
+│ ├── redux/
+│ │ ├── api/
+│ │ │ ├── authApi.ts
+│ │ │ ├── types.ts
+│ │ │ └── userApi.ts
+│ │ ├── features/
+│ │ │ └── userSlice.ts
+│ │ └── store.ts
+│ ├── App.tsx
+│ ├── index.tsx
+│ └── react-app-env.d.ts
+├── .env.local
+├── package.json
+├── README.md
+├── tsconfig.json
+└── yarn.lock
+```
---
+++ src/components/Header.tsx
@@ -0,0 +1,126 @@
+
+import {
+    AppBar,
+    Avatar,
+    Box,
+    Container,
+    IconButton,
+    Toolbar,
+    Tooltip,
+    Typography,
+  } from '@mui/material';
+  import { styled } from '@mui/material/styles';
+  import { useNavigate } from 'react-router-dom';
+  import { useAppSelector } from '../redux/store';
+  import { useLogoutUserMutation } from '../redux/api/authApi';
+  import { useEffect } from 'react';
+  import { toast } from 'react-toastify';
+  import { LoadingButton as _LoadingButton } from '@mui/lab';
+  
+  const LoadingButton = styled(_LoadingButton)`
+    padding: 0.4rem;
+    background-color: #f9d13e;
+    color: #2363eb;
+    font-weight: 500;
+  
+    &:hover {
+      background-color: #ebc22c;
+      transform: translateY(-2px);
+    }
+  `;
+  
+  const Header = () => {
+    const navigate = useNavigate();
+    const user = useAppSelector((state) => state.userState.user);
+  
+    const [logoutUser, { isLoading, isSuccess, error, isError }] =
+      useLogoutUserMutation();
+  
+    useEffect(() => {
+      if (isSuccess) {
+        // window.location.href = '/login';
+        navigate('/login');
+      }
+  
+      if (isError) {
+        if (Array.isArray((error as any).data.error)) {
+          (error as any).data.error.forEach((el: any) =>
+            toast.error(el.message, {
+              position: 'top-right',
+            })
+          );
+        } else {
+          toast.error((error as any).data.message, {
+            position: 'top-right',
+          });
+        }
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isLoading]);
+  
+    const onLogoutHandler = async () => {
+      logoutUser();
+    };
+  
+    return (
+      <AppBar position='static'>
+        <Container maxWidth='lg'>
+          <Toolbar>
+            <Typography
+              variant='h6'
+              onClick={() => navigate('/')}
+              sx={{ cursor: 'pointer' }}
+            >
+              CodevoWeb
+            </Typography>
+            <Box display='flex' sx={{ ml: 'auto' }}>
+              {!user && (
+                <>
+                  <LoadingButton
+                    sx={{ mr: 2 }}
+                    onClick={() => navigate('/register')}
+                  >
+                    SignUp
+                  </LoadingButton>
+                  <LoadingButton onClick={() => navigate('/login')}>
+                    Login
+                  </LoadingButton>
+                </>
+              )}
+              {user && (
+                <LoadingButton
+                  sx={{ backgroundColor: '#eee' }}
+                  onClick={onLogoutHandler}
+                  loading={isLoading}
+                >
+                  Logout
+                </LoadingButton>
+              )}
+              {user && user?.role === 'admin' && (
+                <LoadingButton
+                  sx={{ backgroundColor: '#eee', ml: 2 }}
+                  onClick={() => navigate('/admin')}
+                >
+                  Admin
+                </LoadingButton>
+              )}
+              <Box sx={{ ml: 4 }}>
+                <Tooltip
+                  title='Post settings'
+                  onClick={() => navigate('/profile')}
+                >
+                  <IconButton sx={{ p: 0 }}>
+                    <Avatar alt='Remy Sharp' src='/static/images/avatar/2.jpg' />
+                  </IconButton>
+                </Tooltip>
+              </Box>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    );
+  };
+  
+  export default Header;
+  
+  
